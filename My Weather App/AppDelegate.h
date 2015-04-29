7e6fd00f1edd9699f25451279bc03e263a9fab61
Muhammad Iqbal <Wikkk@Muhammads-Air.default>
2015-04-29 19:46:33 +0100

    Initial Commit

---
+++ My Weather App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Weather App
+//
+//  Created by Muhammad Iqbal on 29/04/2015.
+//  Copyright (c) 2015 Muhammad Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Weather App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Weather App
+//
+//  Created by Muhammad Iqbal on 29/04/2015.
+//  Copyright (c) 2015 Muhammad Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
